d0709a8b93856755b52e52fa1cbac521b560cda5
Ifeanyi Nneji <ifeanyinneji777@gmail.com>
2022-08-22 18:07:40 +0000

    added new folder

---
+++ Pixelizer2/Pixelizer/app.py
@@ -0,0 +1,63 @@
+import io
+import os
+import sys
+
+import cv2
+import numpy as np
+from blur_the_face_video import face_blurring, face_pixelate
+from fastapi import FastAPI, File, Request, UploadFile
+from fastapi.responses import FileResponse
+from PIL import Image
+
+sys.path.append(os.path.abspath(os.path.join("..", "config")))
+
+
+app = FastAPI(
+    title="Face Pixelizer API",
+    description="""An API for Automatic Face Pixellization of Images""",
+)
+
+
+@app.get("/")
+async def running():
+    note = """
+    Face Pixelizer API 📚
+    An API for Automatic Face Pixellization of Images!
+    Note: add "/redoc" to get the complete documentation.
+    """
+    return note
+
+
+# endpoint for just enhancing the image
+@app.post("/pixelize")
+async def face_pixelize(file: UploadFile = File(...)):
+
+    contents = io.BytesIO(await file.read())
+    file_bytes = np.asarray(bytearray(contents.read()), dtype=np.uint8)
+    img = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
+    cv2.imwrite("image.jpg", img)
+    try:
+        image = cv2.imread("image.jpg")
+        images = face_blurring(image, blur_type="pixelate", blocks=20)
+        cv2.imwrite("out.jpg", images)
+        return FileResponse("out.jpg", media_type="image/jpg")
+    except ValueError:
+        vals = "Error! Please upload a valid image type."
+        return vals
+
+
+@app.post("/blur")
+async def face_blur(file: UploadFile = File(...)):
+
+    contents = io.BytesIO(await file.read())
+    file_bytes = np.asarray(bytearray(contents.read()), dtype=np.uint8)
+    img = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
+    cv2.imwrite("image.jpg", img)
+    try:
+        image = cv2.imread("image.jpg")
+        images = face_blurring(image, blur_type="simple", blocks=20)
+        cv2.imwrite("output.jpg", images)
+        return FileResponse("output.jpg", media_type="image/jpg")
+    except ValueError:
+        vals = "Error! Please upload a valid image type."
+        return vals
---
+++ Pixelizer2/Pixelizer/blur_the_face_video.py
@@ -0,0 +1,68 @@
+import time
+import cv2
+import numpy as np
+
+prototxt_path = "models/deploy.prototxt.txt"
+caffemodel_path = "models/weights.caffemodel"
+CONFIDENCE = 0.5
+
+model = cv2.dnn.readNetFromCaffe(prototxt_path, caffemodel_path)
+
+
+def face_pixelate(image, blocks=3):
+    (h, w) = image.shape[:2]
+    Xsteps = np.linspace(0, w, blocks + 1, dtype="int")
+    Ysteps = np.linspace(0, h, blocks + 1, dtype="int")
+    for i in range(1, len(Ysteps)):
+        for j in range(1, len(Xsteps)):
+            startX = Xsteps[j - 1]
+            endX = Xsteps[j]
+            startY = Ysteps[i - 1]
+            endY = Ysteps[i]
+            roi = image[startY:endY, startX:endX]
+            (B, G, R) = [int(x) for x in cv2.mean(roi)[:3]]
+            res = cv2.rectangle(image, (startX, startY), (endX, endY), (B, G, R), -1)
+    return image
+
+
+def face_blurring(image, blur_type, blocks=3):
+    img_copy = np.copy(image)
+    (h, w) = img_copy.shape[:2]
+    blob = cv2.dnn.blobFromImage(
+        cv2.resize(img_copy, (300, 300)), 1.0, (300, 300), (104.0, 177.0, 123.0)
+    )
+    model.setInput(blob)
+    detections = model.forward()
+    for i in range(0, detections.shape[2]):
+        confidence = detections[0, 0, i, 2]
+        if confidence > CONFIDENCE:
+            box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+            (startX, startY, endX, endY) = box.astype("int")
+            face = img_copy[startY:endY, startX:endX]
+            if blur_type == "pixelate":
+                face = face_pixelate(face, blocks)
+                img_copy[startY:endY, startX:endX] = face
+            else:
+                face = cv2.blur(face, (23, 23))
+                img_copy[startY:endY, startX:endX] = face
+    return img_copy
+
+
+def video_blurring(video, blur_type, blocks, path):
+    cap = cv2.VideoCapture(video)
+    time.sleep(2.0)
+    res, frame = cap.read()
+    h, w = frame.shape[:2]
+    fourcc = cv2.VideoWriter_fourcc(*"XVID")
+    out = cv2.VideoWriter(path, fourcc, 20.0, (w, h))
+    while True:
+        res, frame = cap.read()
+        if frame is None:
+            break
+        output = face_blurring(frame, blur_type, blocks)
+        out.write(output)
+        # if cv2.waitKey(1) & 0xFF==ord('a'):
+        #    break
+    cap.release()
+    out.release()
+    cv2.destroyAllWindows()
