131d2c04d48aabe231e3eb9e16585de22ed3b919
Ifeanyi Nneji <ifeanyinneji777@gmail.com>
2022-08-22 18:03:01 +0000

    updatedupdated gitignore and added new files

---
+++ Colorizer/download.py
@@ -0,0 +1,17 @@
+import gdown
+
+def url_to_id(url):
+    x = url.split("/")
+    return x[5]
+
+id = url_to_id('https://drive.google.com/file/d/1EshSEknFNC0eknpyLk39N1x-PKRi4Vpv/view?usp=sharing')
+
+
+def main():
+
+    url = 'https://drive.google.com/uc?id='+id
+    output = 'models/colorization_release_v2.caffemodel'
+    gdown.download(url, output, quiet=False)
+
+if __name__=="__main__":
+    main()
