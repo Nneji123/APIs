a5547e0a235240b3e0dbf334106fa04ab054fe98
Ifeanyi Nneji <ifeanyinneji777@gmail.com>
2022-08-24 22:44:18 +0000

    nnn

--- violence_detection_api/download.py
+++ violence_detection_api/download.py
@@ -1,12 +1,13 @@
 import os
-from model import classify_image
-from PIL import Image
-import numpy as np
-if os.path.exists("bestmodel.h5"):
-    print("This file exists!")
-else:
+try:
+    from model import classify_image
+except OSError as e:
     os.system("wget -O bestmodel.h5 https://huggingface.co/spaces/SIB/violence_api/resolve/main/bestmodel.h5")
     print("File downloaded!")
+    from model import classify_image
+from PIL import Image
+import numpy as np
+
 
 image = Image.open("images.jpeg")
 image = image.resize((224, 224), Image.ANTIALIAS)
