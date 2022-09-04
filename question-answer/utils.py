e6008c93d32b9516bfbc5532fa3effd2c0168399
Ifeanyi Nneji <ifeanyinneji777@gmail.com>
2022-09-04 17:41:09 +0000

    last commit for question-answering api

--- question-answer/utils.py
+++ question-answer/utils.py
@@ -6,22 +6,6 @@ model_name = "deepset/roberta-base-squad2"
 nlp = pipeline('question-answering', model=model_name, tokenizer=model_name)
 
 def get_answer(question:str, context:str):
-    """
-    The get_answer function takes a question and context as input, and returns the answer string
-    and score of the best answer found in the context. The function first tokenizes both question
-    and context using spacy's nlp object, then passes them to pipeline.get_answer which returns 
-    the answer string and score for each sentence in the document.
-    
-    Args:
-        question:str: Store the question string
-        context:str: Pass the context of the question
-    
-    Returns:
-        A dictionary with the answer and score
-    
-    Doc Author:
-        Ifeanyi
-    """
     QA_input = {
         'question': question,
         'context': context
@@ -32,4 +16,3 @@ def get_answer(question:str, context:str):
     vals = {"answer": ans, "score": round(scores, 2)}
     return vals
 
-#print(get_answer("What is the name of the first president of the United States?", "George Washington was the first president of the United States of America."))
