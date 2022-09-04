c930eb6575f67d96ac6c6d78403a5f6d072d8fb7
Ifeanyi Nneji <ifeanyinneji777@gmail.com>
2022-09-04 20:31:17 +0000

    updated files

---
+++ neural-style-transfer/app.py
@@ -0,0 +1,54 @@
+import io
+
+import cv2
+import numpy as np
+from fastapi import FastAPI, File, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import FileResponse, PlainTextResponse
+
+from utils import inference
+
+app = FastAPI(
+    title="Neural Style Transfer API",
+    description="""An API for transferring styles of images.""",
+)
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+
+@app.get("/", response_class=PlainTextResponse, tags=["home"])
+async def home():
+    note = """
+    Neural Style Transfer API!
+    Note: add "/redoc" to get the complete documentation.
+    """
+    return note
+
+
+@app.post("/style-transfer", tags=["style-transfer"], description="Transfer style of an image. input_image is the image to be styled and style_image is the image to be used as style.")
+async def get_image(input_image: UploadFile = File(...), style_image: UploadFile = File(...)):
+
+    contents = io.BytesIO(await input_image.read())
+    file_bytes = np.asarray(bytearray(contents.read()), dtype=np.uint8)
+    img = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
+    cv2.imwrite("image.jpg", img)
+    contents2 = io.BytesIO(await style_image.read())
+    file_bytes2 = np.asarray(bytearray(contents2.read()), dtype=np.uint8)
+    img2 = cv2.imdecode(file_bytes2, cv2.IMREAD_COLOR)
+    cv2.imwrite("image2.jpg", img2)
+    try:
+        inference()
+        return FileResponse("output.jpg", media_type="image/jpeg")
+    except ValueError as e:
+        e = "Error! Please upload a valid image type."
+        return e 
+
---
+++ neural-style-transfer/utils.py
@@ -0,0 +1,31 @@
+import os
+from datetime import datetime
+
+import cv2
+import numpy as np
+import paddlehub as hub
+from PIL import Image
+import warnings
+warnings.filterwarnings("ignore")
+
+
+os.system("hub install stylepro_artistic==1.0.1")
+stylepro_artistic = hub.Module(name="stylepro_artistic")
+
+def inference():
+    image_style = Image.open("image.jpg").convert("RGB")
+    image_style = np.array(image_style)
+    image_style = image_style[:, :, ::-1].copy() 
+    content_image = image_style
+    start=datetime.now()
+
+    result = stylepro_artistic.style_transfer(
+        images=[{
+            "content": content_image,
+            "styles": [cv2.imread("image2.jpg")]
+        }])
+
+    print(f"Time spent at Style Transfer: {datetime.now()-start}")
+    img = Image.fromarray(np.uint8(result[0]["data"])[:,:,::-1]).convert("RGB")
+    img.resize((512,512),Image.ANTIALIAS).save("output.jpg")
+    return "output.jpg"
