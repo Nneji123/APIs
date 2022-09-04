3c4d25fa900db14631a4c022ec1eea8f7a419605
Ifeanyi Nneji <ifeanyinneji777@gmail.com>
2022-09-04 17:07:13 +0000

    updated files

--- question-answer/app.py
+++ question-answer/app.py
@@ -1,3 +1,5 @@
+import os
+import PyPDF2
 from fastapi import FastAPI, File, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import PlainTextResponse
@@ -28,27 +30,69 @@ async def home():
     This is a fastapi server that can answerr questions.
     Note: add "/redoc" to get the complete documentation.
     """
-    return 
+    return note
 
 class Question(BaseModel):
     question: str
     context: str
+    
+    class Config:
+        schema_extra = {
+            "example": {
+            "question": "What is the name of the first president of the United States?",
+            "context": "George Washington was the first president of the United States of America."
+        }
+        }
 
-
-@app.post("/question-answer")
-async def get_answer(data: Question):
+@app.post("/question-answer", tags=["question-answer"])
+async def get_answer_text(data: Question):
     try:
-        answer = get_answer(data.question,data.context)
+        answer = get_answer(question = data.question, context = data.context)
+        if answer == "":
+            return {"answer": "No answer found"}
         return answer
     except Exception as e:
         e = "Cannot answer context."
         return e
 
-@app.post("/question-answer-file")
-async def get_answer_file(data:Question, file: UploadFile = File(...)):
-    contents = await file.read()
+@app.post("/question-answer-file", tags=["text-file"], description="Upload a text file to answer questions.")
+async def get_answer_textfile(myquestion:str, file: UploadFile = File(...)):
+    files = await file.read()
+    # save the file
+    filename = "file.txt"
+    with open(filename, "wb+") as f:
+        f.write(files)
     try:
-        answer = get_answer(data.question,contents)
+        with open(filename, "r") as f:
+            txt = f.read()
+        answer = get_answer(question = myquestion, context = txt)
+        if os.path.exists("file.txt"):
+            os.remove("file.txt")
+        if answer == "":
+            answer = "Cannot answer context."
+        return answer
+    except Exception as e:
+        e = "Cannot answer context."
+        return e
+    
+    
+@app.post("/question-answer-pdf", tags=["pdf-file"], description="Upload a pdf file to answer questions.")
+async def get_answer_pdf(myquestion:str, file: UploadFile = File(...)):
+    files = await file.read()
+    # save the file
+    filename = "file.pdf"
+    with open(filename, "wb+") as f:
+        f.write(files)
+    try:
+        with open("file.pdf", "rb") as pdf:
+            pdfReader = PyPDF2.PdfFileReader(pdf)
+            pageObj = pdfReader.getPage(0)
+            pdftext=pageObj.extractText()
+        answer = get_answer(question = myquestion, context = pdftext)
+        if os.path.exists("file.pdf"):
+            os.remove("file.pdf")
+        if answer == "":
+            answer = "Cannot answer context."
         return answer
     except Exception as e:
         e = "Cannot answer context."
